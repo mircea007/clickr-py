368c4209b36ddb8af6bb8ee783cc86375477f4ed
mircea_007 <mirceareb@gmail.com>
2022-06-10 17:17:56 +0300

    works with both buttons, need to press END to close

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import time
 import threading
-import random     # to throw off bot detection
-import pynput.mouse as mouselib
-import pynput.keyboard as kblib
+import random                   # to throw off bot detection
+import pynput.mouse as mouselib # mouse library
+import pynput.keyboard as kblib # keyboard library
+import sys
 
 mouse = mouselib.Controller()
 
@@ -13,13 +14,13 @@ class AutoClicker:
     statecpy = 0
 
     self.state_mutex.acquire()
-    while self.state != 0:
+    while self.state:
       statecpy = self.state - 1 # keep copy to release mutex
       self.state_mutex.release()
       
       if statecpy:
-        mouse.press( mouselib.Button.left )
-        mouse.release( mouselib.Button.left )      
+        mouse.press( self.button )
+        mouse.release( self.button )      
 
       self.cps_mutex.acquire()
       delay = random.uniform( self.min_delay, self.max_delay )
@@ -31,12 +32,10 @@ class AutoClicker:
     self.state_mutex.release()
 
   def __init__( self, button, cps = 10.0, delta = 0.3 ):
-    self.cmd = [ "xdotool", "click", str( button + 1 ) ] # get command line for button
-    self.thread = threading.Thread( target = AutoClicker.thread_proc, args = (self,) ) # start worker thread
-
     # autoclicker state
     self.state_mutex = threading.Lock()
     self.state = 1 # 0 - exit, 1 - not running, 2 - running
+    self.button = button
     
     # clicking speed
     self.cps_mutex = threading.Lock()
@@ -44,6 +43,10 @@ class AutoClicker:
     self.cps = cps
 
     # start worker thread
+    self.thread = threading.Thread(
+      target = AutoClicker.thread_proc,
+      args = (self,)
+    )
     self.thread.start()
     
   # cps control
@@ -71,15 +74,11 @@ class AutoClicker:
 
   # state control
   def start( self ):
-    print( "start called" )
-
     self.state_mutex.acquire()
     self.state = 2
     self.state_mutex.release()
   
   def stop( self ):
-    print( "stop called" )
-
     self.state_mutex.acquire()
     self.state = 1
     self.state_mutex.release()
@@ -91,46 +90,81 @@ class AutoClicker:
 
     self.thread.join()
 
-auto = AutoClicker( 0, cps = 10.0, delta = 0 )
+auto_left = AutoClicker( mouselib.Button.left )
+auto_right = AutoClicker( mouselib.Button.right )
+# not doing middle click (because why???)
 
-mouses = 0 # number of mouses pressed at one time
+def ragequit():
+  global auto_left
+  global auto_right
+  global kb_listener
+  global mouse_listener
+
+  auto_left.end()
+  auto_right.end()
+  kb_listener.stop()
+  mouse_listener.stop()
+
+  sys.exit( 0 )
+
+# number of mouses pressed at one time
+mouses_left = 0
+mouses_right = 0
+
 caps_lock = False
 caps_ignore = False
 state_mutex = threading.Lock()
 
-prev_mouse_state = False
+prev_mouse_state_left = False
+prev_mouse_state_right = False
 
 # when calling this function make sure state_mutex is locked
 def recalc_state():
-  global mouses
+  global mouses_left
+  global mouses_right
   global caps_lock
-  global prev_mouse_state
+  global prev_mouse_state_left
+  global prev_mouse_state_right
   global auto
 
-  print( "mouses = " + str( mouses ) + " | caps_lock = " + str( caps_lock ) )
+  mouse_state_left = (mouses_left > 0) and caps_lock
+  mouse_state_right = (mouses_right > 0) and caps_lock
 
-  mouse_state = (mouses > 0) and caps_lock
-
-  if mouse_state != prev_mouse_state:
-    if mouse_state:
-      auto.start()
+  if mouse_state_left != prev_mouse_state_left:
+    if mouse_state_left:
+      auto_left.start()
     else:
-      auto.stop()
+      auto_left.stop()
   
-  prev_mouse_state = mouse_state
+  if mouse_state_right != prev_mouse_state_right:
+    if mouse_state_right:
+      auto_right.start()
+    else:
+      auto_right.stop()
+
+  prev_mouse_state_left = mouse_state_left
+  prev_mouse_state_right = mouse_state_right
 
 def on_click( x, y, button, pressed ):
   global state_mutex
-  global mouses
+  global mouses_left
+  global mouses_right
 
   state_mutex.acquire()
 
-  mouses += (1 if pressed else -1)
+  if button == mouselib.Button.left:
+    mouses_left += (1 if pressed else -1)
+  elif button == mouselib.Button.right:
+    mouses_right += (1 if pressed else -1)
+
   recalc_state()
 
   state_mutex.release()
 
 def on_press( key ):
+  if key == kblib.Key.end:
+    ragequit()
+
   if key == kblib.Key.caps_lock:
     global state_mutex
     global caps_lock
@@ -144,6 +178,10 @@ def on_press( key ):
       recalc_state()
 
     state_mutex.release()
+
+def test_handle( sig, frame ):
+  print( "yo!!!" )
+  exit( 1 )
 
 kb_listener = kblib.Listener( on_press = on_press )
 kb_listener.start()
